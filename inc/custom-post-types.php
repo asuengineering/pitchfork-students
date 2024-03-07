054253105bee281191f0e56356c9393cb23085fc
Steve Ryan <steve.ryan@asu.edu>
2024-03-07 10:46:05 -0700

    Add orgtype taxonomy definition

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -18,35 +18,35 @@ function pitchfork_students_studentorg_cpt() {
 		'menu_name'             => __( 'Student Orgs', 'pitchfork-students' ),
 		'name_admin_bar'        => __( 'Student Orgs', 'pitchfork-students' ),
 		'archives'              => __( 'Student Orgs', 'pitchfork-students' ),
-		'attributes'            => __( 'Org attributes', 'pitchfork-students' ),
-		'parent_item_colon'     => __( 'Parent Org:', 'pitchfork-students' ),
-		'all_items'             => __( 'All Orgs', 'pitchfork-students' ),
-		'add_new_item'          => __( 'Add New Org', 'pitchfork-students' ),
+		'attributes'            => __( 'Student Org attributes', 'pitchfork-students' ),
+		'parent_item_colon'     => __( 'Parent Student Org:', 'pitchfork-students' ),
+		'all_items'             => __( 'All Student Orgs', 'pitchfork-students' ),
+		'add_new_item'          => __( 'Add New Student Org', 'pitchfork-students' ),
 		'add_new'               => __( 'Add New', 'pitchfork-students' ),
-		'new_item'              => __( 'New Org', 'pitchfork-students' ),
-		'edit_item'             => __( 'Edit Org', 'pitchfork-students' ),
-		'update_item'           => __( 'Update Org', 'pitchfork-students' ),
-		'view_item'             => __( 'View Org', 'pitchfork-students' ),
-		'view_items'            => __( 'View Orgs', 'pitchfork-students' ),
-		'search_items'          => __( 'Search Org', 'pitchfork-students' ),
+		'new_item'              => __( 'New Student Org', 'pitchfork-students' ),
+		'edit_item'             => __( 'Edit Student Org', 'pitchfork-students' ),
+		'update_item'           => __( 'Update Student Org', 'pitchfork-students' ),
+		'view_item'             => __( 'View Student Org', 'pitchfork-students' ),
+		'view_items'            => __( 'View Student Orgs', 'pitchfork-students' ),
+		'search_items'          => __( 'Search Student Org', 'pitchfork-students' ),
 		'not_found'             => __( 'Not found', 'pitchfork-students' ),
 		'not_found_in_trash'    => __( 'Not found in Trash', 'pitchfork-students' ),
 		'featured_image'        => __( 'Featured Image', 'pitchfork-students' ),
 		'set_featured_image'    => __( 'Set featured image', 'pitchfork-students' ),
 		'remove_featured_image' => __( 'Remove featured image', 'pitchfork-students' ),
 		'use_featured_image'    => __( 'Use as featured image', 'pitchfork-students' ),
-		'insert_into_item'      => __( 'Insert into Org', 'pitchfork-students' ),
-		'uploaded_to_this_item' => __( 'Uploaded to this org', 'pitchfork-students' ),
-		'items_list'            => __( 'Orgs list', 'pitchfork-students' ),
-		'items_list_navigation' => __( 'Orgs list navigation', 'pitchfork-students' ),
-		'filter_items_list'     => __( 'Filter orgs list', 'pitchfork-students' ),
+		'insert_into_item'      => __( 'Insert into Student Org', 'pitchfork-students' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this student org', 'pitchfork-students' ),
+		'items_list'            => __( 'Student Orgs list', 'pitchfork-students' ),
+		'items_list_navigation' => __( 'Student Orgs list navigation', 'pitchfork-students' ),
+		'filter_items_list'     => __( 'Filter student orgs list', 'pitchfork-students' ),
 	);
 	$args = array(
 		'label'                 => __( 'Student Org', 'pitchfork-students' ),
 		'description'           => __( 'A student organization', 'pitchfork-students' ),
 		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions' ),
-		'taxonomies'            => array( 'category' ),
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions', 'excerpt' ),
+		'taxonomies'            => array( 'category', 'orgtype' ),
 		'hierarchical'          => false,
 		'public'                => true,
 		'show_ui'               => true,
@@ -66,3 +66,50 @@ function pitchfork_students_studentorg_cpt() {
 
 }
 add_action( 'init', 'pitchfork_students_studentorg_cpt', 0 );
+add_theme_support( 'post-thumbnails' );
+
+
+/**
+ * Register custom post type for student organizations
+ * SLUG: orgtype
+ * CPT: studentorg
+ */
+function pitchfork_students_orgtype_tax() {
+
+	$labels = array(
+		'name'                       => _x( 'Student Org Types', 'Taxonomy General Name', 'pitchfork-students' ),
+		'singular_name'              => _x( 'Student Org Type', 'Taxonomy Singular Name', 'pitchfork-students' ),
+		'menu_name'                  => __( 'Org Type', 'pitchfork-students' ),
+		'all_items'                  => __( 'All Org Types', 'pitchfork-students' ),
+		'parent_item'                => __( 'Parent Org Type', 'pitchfork-students' ),
+		'parent_item_colon'          => __( 'Parent Org Type:', 'pitchfork-students' ),
+		'new_item_name'              => __( 'New Org Type Name', 'pitchfork-students' ),
+		'add_new_item'               => __( 'Add New Org Type', 'pitchfork-students' ),
+		'edit_item'                  => __( 'Edit Org Type', 'pitchfork-students' ),
+		'update_item'                => __( 'Update Org Type', 'pitchfork-students' ),
+		'view_item'                  => __( 'View Org Type', 'pitchfork-students' ),
+		'separate_items_with_commas' => __( 'Separate types with commas', 'pitchfork-students' ),
+		'add_or_remove_items'        => __( 'Add or remove types', 'pitchfork-students' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'pitchfork-students' ),
+		'popular_items'              => __( 'Popular Org Types', 'pitchfork-students' ),
+		'search_items'               => __( 'Search Org Types', 'pitchfork-students' ),
+		'not_found'                  => __( 'Not Found', 'pitchfork-students' ),
+		'no_terms'                   => __( 'No org types', 'pitchfork-students' ),
+		'items_list'                 => __( 'Org types list', 'pitchfork-students' ),
+		'items_list_navigation'      => __( 'Org types navigation', 'pitchfork-students' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => false,
+		'show_in_rest'               => true,
+	);
+	register_taxonomy( 'orgtype', array( 'studentorg' ), $args );
+
+}
+add_action( 'init', 'pitchfork_students_orgtype_tax', 0 );
+
